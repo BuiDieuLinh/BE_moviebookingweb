d4906e3c6facb5c871d10da54bf638fdd01757f7
BuiDieuLinh <buithidieulinh.tlnh@gmail.com>
2025-04-10 02:46:05 +0700

    test payment by momo and vnpay

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "vnp_TmnCode":"QF9M1HRI",
+    "vnp_HashSecret":"TLMZMJNEK0KZXU8EOGRHYV75GS834PCS",
+    "vnp_Url":"https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
+    "vnp_Api":"https://sandbox.vnpayment.vn/merchant_webapi/api/transaction",
+    "vnp_ReturnUrl": "http://localhost:3000/payment"
+  }
--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,5 +1,20 @@
 const payment = require("../models/payment.model");
-const axios = require('axios')
+const axios = require('axios');
+const request = require('request');
+const moment = require('moment');
+const crypto = require('crypto');
+const querystring = require('qs');
+const config = require('../config/config');
+function sortObject(obj) {
+  let sorted = {};
+  let keys = Object.keys(obj).sort();
+
+  for (let key of keys) {
+      sorted[key] = obj[key];
+  }
+
+  return sorted;
+}
 
 module.exports = {
   getAll: (req, res) => {
@@ -45,9 +60,9 @@ module.exports = {
     var orderId = requestId;
     var orderInfo = `thanh toan don ve ${orderId}`; // nội dung giao dịch
     var redirectUrl = "https://momo.vn/return";
-    var ipnUrl = "https://callback.url/notify";
+    var ipnUrl = "http://localhost:5000/payment";
     // var ipnUrl = redirectUrl = "https://webhook.site/454e7b77-f177-4ece-8236-ddf1c26ba7f8";
-    var amount = "0";
+    var amount = "10000";
     var requestType = "captureWallet"
     var extraData = ""; //pass empty value if your merchant does not have stores
 
@@ -103,8 +118,66 @@ module.exports = {
     }
   },
 
-  callback: async (req, res) =>{
-    console.log("callback: ")
+  paymentBy_vnpay: async (req, res) => {
+    process.env.TZ = 'Asia/Ho_Chi_Minh';
+    
+    let date = new Date();
+    let createDate = moment(date).format('YYYYMMDDHHmmss');
+    
+    let ipAddr = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+
+    // Cấu hình từ tệp config
+    let tmnCode = config.vnp_TmnCode;
+    let secretKey = config.vnp_HashSecret;
+    let vnpUrl = config.vnp_Url;
+    let returnUrl = config.vnp_ReturnUrl;
+    
+    // Thông tin đơn hàng
+    let orderId = moment(date).format('DDHHmmss');
+    let amount = req.body.amount;
+    let bankCode = req.body.bankCode;
+    
+    let locale = req.body.language || 'vn';
+    let currCode = 'VND';
+
+    let vnp_Params = {};
+    vnp_Params['vnp_Version'] = '2.1.0';
+    vnp_Params['vnp_Command'] = 'pay';
+    vnp_Params['vnp_TmnCode'] = tmnCode;
+    vnp_Params['vnp_Locale'] = locale;
+    vnp_Params['vnp_CurrCode'] = currCode;
+    vnp_Params['vnp_TxnRef'] = orderId;
+    vnp_Params['vnp_OrderInfo'] = 'Thanh toan cho ma GD:' + orderId;
+    vnp_Params['vnp_OrderType'] = 'other';
+    vnp_Params['vnp_Amount'] = amount * 100; // chuyển sang đơn vị tiền tệ (VND)
+    vnp_Params['vnp_ReturnUrl'] = returnUrl;
+    vnp_Params['vnp_IpAddr'] = ipAddr;
+    vnp_Params['vnp_CreateDate'] = createDate;
+
+    // Thêm mã ngân hàng nếu có
+    if (bankCode) {
+        vnp_Params['vnp_BankCode'] = bankCode;
+    }
+
+    // Sắp xếp tham số theo thứ tự chữ cái
+    vnp_Params = sortObject(vnp_Params);
+
+    // Tạo chuỗi querystring từ các tham số đã sắp xếp
+    let signData = querystring.stringify(vnp_Params, { encode: false });
+    
+    // Tạo chữ ký HMAC với secretKey
+    let hmac = crypto.createHmac("sha512", secretKey);
+    let signed = hmac.update(Buffer.from(signData, 'utf-8')).digest("hex");
+    vnp_Params['vnp_SecureHash'] = signed;
+
+    // Tạo URL thanh toán
+    vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+
+    // Chuyển hướng người dùng tới trang thanh toán
+    res.redirect(vnpUrl);
   }
 };
 
